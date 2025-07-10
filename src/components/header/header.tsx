9a55cfe90f2dd2d9afe497e03d85dafeaeb5a970
Webis-2022 <verjovkin.jevgeni@gmail.com>
2025-07-10 11:32:52 +0300

    feat: add header component

---
+++ src/components/header/header.tsx
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+
+export class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1 className="app-title">Find Your Star Wars Hero</h1>
+      </header>
+    );
+  }
+}
