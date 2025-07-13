f8b62b6fd6b6bef8ca5634ae0d9605cfa38776c0
Webis-2022 <verjovkin.jevgeni@gmail.com>
2025-07-13 20:00:21 +0300

    feat: add bundle

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { ghPages } from 'vite-plugin-gh-pages';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/REACT2025Q3/class-components/',
+  plugins: [
+    react(),
+    ghPages({
+      branch: 'gh-pages',
+      repo: 'https://github.com/Webis-2022/REACT2025Q3.git',
+      dir: 'dist',
+      dotfiles: true,
+      push: true,
+    }),
+  ],
 });
