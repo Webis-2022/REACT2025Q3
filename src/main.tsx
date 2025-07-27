e883cbf353431e2fb20911f29250db1017756ae6
Webis-2022 <verjovkin.jevgeni@gmail.com>
2025-07-28 00:06:30 +0300

    fix: add HashRouter instead of BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { App } from './App.tsx';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const rootEl = document.getElementById('root');
 if (rootEl) {
   createRoot(rootEl).render(
     <StrictMode>
-      <BrowserRouter basename="/REACT2025Q3/">
+      <HashRouter basename="/REACT2025Q3/">
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </StrictMode>
   );
 }
